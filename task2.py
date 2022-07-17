a3b7bdf57d12d9ae65b6d82e4f9c621aad660563
Tatyana <android87@list.ru>
2022-07-17 19:43:29 +0400

    add check

--- task2.py
+++ task2.py
@@ -2,13 +2,22 @@
 # *Пример:*
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-number = int(input('enter N: '))
 
-if number < 0:
-    number = number * -1
+
+def inputNo():
+    n = input('введите целое положительное число N: ')
+    return n   
+
+def check_if_number(num):
+    while not num.isdigit():
+        num = inputNo()
+    return (int( num ))
+
+number = check_if_number(inputNo())
+
 
 list_of_nums = [1]
-for i in range(1,number):
+for i in range(1, number):
     list_of_nums.append(list_of_nums[i-1] * (i+1))
 print(list_of_nums)
 
